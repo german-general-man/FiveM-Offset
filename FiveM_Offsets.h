22b9c7b6b352d43f66ca36b37220c8026684fa86
Blanco ☭ <108994641+Wayzq-00001@users.noreply.github.com>
2023-02-10 21:09:01 +0100

    Offset Update

--- FiveM_Offsets.h
+++ FiveM_Offsets.h
@@ -1,45 +1,45 @@
 #pragma once
 #include <windows.h>
 
-// Offsets found on 1.02.2023 by MoppyBlack (FiveM Unban God)
-// My Discord-Tag: M0ppxBIaCK#2462
+// Offsets found on 10.02.2023 by MoppyBlack (FiveM Unban God)
+// My Discord-Tag: Juan.#8888
 
 namespace offset 
 {
-    // net.dll 
-    uintptr_t serverIP = 0x16630 // GetAddress
-    uintptr_t getport = 0x169e0 // GetPort
-    uintptr_t steam_name = 0x14d20 // NetAddress
-    uintptr_t ICoreGameInit = 0x40880; // ICoreGameInit and then Get (void)
+    //net.dll 
+    uintptr_t serverIP = 0x168e0 // GetAddress
+    uintptr_t getport = 0x16ca0 // GetPort
+    uintptr_t steam_name = 0x14e90 // NetAddress
+    uintptr_t ICoreGameInit = 0x40f80; // ICoreGameInit and then Get (void)
 
-    // conhost-v2.dll
-    uintptr_t c_print = 0x2fb40 //ConHost::Print
-    uintptr_t c_open = 0x2f310 //ConHost::IsConsoleOpen
+    //conhost-v2.dll
+    uintptr_t c_print = 0x2faa0 //ConHost::Print
+    uintptr_t c_open = 0x2f270 //ConHost::IsConsoleOpen
 
     //discord.dll
-    uintptr_t discordmanager = 0x58120 // discordAppId
+    uintptr_t discordmanager = 0x58100 // discordAppId
 
     //citizen_resources_core.dll
-    uintptr_t resource_name = 0x16470 // GetName
-    uintptr_t resource_path = 0x164a0 // GetPath
-    uintptr_t resource_state = 0x164c0// GetState
-    uintptr_t resource_manager = 0x16490 // GetManager ResourceImpl
-    uintptr_t resource_manager2 = 0x101b0 // GetManager ResourceEventComponent
-    uintptr_t resource_core = 0x222e0 // GetResource ResourceManagerImpl
-    uintptr_t resource_core2 = 0x164b0 // GetResource ResourceMetaDataComponent
-    uintptr_t resource_stop = 0x16b20 // Stop
-    uintptr_t resource_start = 0x16a50 // Start
-    uintptr_t bypassAddress = 0x34ae0 // LoadMetaData
+    uintptr_t resource_name = 0x164c0 // GetName
+    uintptr_t resource_path = 0x164f0 // GetPath
+    uintptr_t resource_state = 0x16510 // GetState
+    uintptr_t resource_manager = 0x164e0 // GetManager ResourceImpl
+    uintptr_t resource_manager2 = 0x103a0 // GetManager ResourceEventComponent
+    uintptr_t resource_core = 0x225e0 // GetResource ResourceManagerImpl
+    uintptr_t resource_core2 = 0x16500 // GetResource ResourceMetaDataComponent
+    uintptr_t resource_stop = 0x16b70 // Stop
+    uintptr_t resource_start = 0x16aa0 // Start
+    uintptr_t bypassAddress = 0x34de0 // LoadMetaData
 
     //citizen_scripting_lua.dll
-    uintptr_t internal_file = 0x19160 // RunFileInternal
-    uintptr_t file_internal = 0x18350 // LoadFileInternal
-    uintptr_t system_file_internal = 0x19670 // LoadSystemFileInternal
-    uintptr_t natives_build = 0x17bb0 // LoadNativesBuild
-    uintptr_t LoadFile = 0x19690 // LoadFile
-    uintptr_t DoFile = 0x30cc0 // DoFile
-    uintptr_t Dofilecont = 0x30ca0 // Dofilecont
-    uintptr_t runtime = 0x19690 //LuaScriptRuntime
+    uintptr_t internal_file = 0x18fc0 // RunFileInternal
+    uintptr_t file_internal = 0x181b0 // LoadFileInternal
+    uintptr_t system_file_internal = 0x18f20 // LoadSystemFileInternal
+    uintptr_t natives_build = 0x17a10 // LoadNativesBuild
+    uintptr_t LoadFile = 0x194f0 // LoadFile
+    uintptr_t DoFile = 0x31470 // DoFile
+    uintptr_t Dofilecont = 0x31450 // Dofilecont
+    uintptr_t runtime = 0x10800 //LuaScriptRuntime
 
     //citizen_resources_metadata_lua.dll
     uintptr_t LoadFile = 0x71b60 // LoadFile
